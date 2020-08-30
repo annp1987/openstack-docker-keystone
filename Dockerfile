630b870ae8fa527ca9806cdc57e6c3bfcea77993
Nguyen Phuong An <annp.cs51@gmail.com>
2020-08-30 23:03:13 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:20.04
+
+EXPOSE 5000 
+ENV KEYSTONE_ADMIN_PASSWORD abc123
+ENV CONTROLLER localhost
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get -y update && apt-get install -y keystone apache2 libapache2-mod-wsgi-py3
+RUN mkdir -p /etc/keystone
+COPY keystone.conf /etc/keystone/keystone.conf
+COPY bootstrap.sh /bootstrap.sh
+CMD sh -x /bootstrap.sh
---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+
+groupadd keystone
+useradd -m -g keystone keystone
+mkdir -p /etc/keystone/jws-keys
+mkdir -p /etc/keystone/jws-keys/public
+mkdir -p /etc/keystone/jws-keys/private
+chown -R keystone:keystone /etc/keystone/jws-keys/
+keystone-manage token_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage create_jws_keypair --keystone-user keystone --keystone-group keystone
+mv private.pem /etc/keystone/jws-keys/private
+mv public.pem /etc/keystone/jws-keys/public
+
+keystone-manage bootstrap --bootstrap-password $KEYSTONE_ADMIN_PASSWORD \
+  --bootstrap-admin-url http://$CONTROLLER:35357/v3/ \
+  --bootstrap-internal-url http://$CONTROLLER:5000/v3/ \
+  --bootstrap-public-url http://$CONTROLLER:5000/v3/ \
+  --bootstrap-region-id RegionOne
+
+echo "ServerName $CONTROLLER" >> /etc/apache2/apache2.conf
+
+cat > /openrc <<EOF
+export OS_USERNAME=admin
+export OS_PASSWORD=$KEYSTONE_ADMIN_PASSWORD
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://$CONTROLLER:5000/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
+
+a2ensite keystone
+apache2ctl -D FOREGROUND
